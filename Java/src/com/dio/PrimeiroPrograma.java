e8a2219fc1a1986a1922a98722b80f752909dd9f
Débora Santos Gaspar <deborasgaspar21@gmail.com>
2022-07-09 16:35:57 -0300

    java primeiros passos dio

---
+++ Java/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+		
+		int a = 2;
+		int b = 5;
+		
+		System.out.println(a+b);
+
+	}
+
+}
---
+++ Java/src/module-info.java
@@ -0,0 +1,2 @@
+module bootcampdio {
+}
